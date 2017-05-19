75df55636de796152198623f35ecfeb1a6df0856
codeaj <codea_mac_05@CodeaCamp05s-Mac-mini.local>
2017-05-19 09:50:56 -0500

    abuelita completa

---
+++ abuelita.rb
@@ -0,0 +1,12 @@
+
+def deaf_grandma(state)
+  if state == "scream"
+    p "NO, NO DESDE 1983"
+  elsif state == "go"
+    p "BYE TQM"
+  else
+    p "HUH?! NO TE ESCUCHO, HIJO!"
+  end   
+end
+
+deaf_grandma("go")
